147e5130ac29fbfcd218900aa6d5c0d9dd84f1fe
Gerhard <brgerd@gmx.at>
2013-12-01 23:01:20 +0100

    Ändern des unicorn.pid Verzeichnisses

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -16,7 +16,7 @@ set -e
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/ruby_projects/weihnachten/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
+PID=$APP_ROOT/tmp/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=gerhard
 #INIT_CONF=$APP_ROOT/config/init.conf
