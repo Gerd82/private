969cb811568bedd8ab909a0bef3f82edd0eb0158
Gerhard <brgerd@gmx.at>
2013-12-01 12:56:54 +0100

    Unicorn App Pfad ändern

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -15,7 +15,7 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/ruby_projects/beispielanwendung
+APP_ROOT=/ruby_projects/weihnachten
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="$APP_ROOT/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=gerhard
