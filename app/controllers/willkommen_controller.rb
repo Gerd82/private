17f7795b04f1860e883381e61bcc1406b26e898a
Gerhard <brgerd@gmx.at>
2013-12-02 16:23:26 +0100

    Hinzufügen einer Willkommen Seite

---
+++ app/controllers/willkommen_controller.rb
@@ -0,0 +1,2 @@
+class WillkommenController < ApplicationController
+end
---
+++ test/controllers/willkommen_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class WillkommenControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
